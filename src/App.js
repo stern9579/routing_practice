5bbf2f333e211e253af65da15055e43e6c0903ca
Andrew Stern <andrewdstern16@gmail.com>
2022-03-21 13:23:41 -0600

    Enter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Link,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <h1>Welcome</h1>
+  )
+}
+const Hello = () => {
+  const {input} = useParams();
+  const {color1} = useParams();
+  console.log(color1);
+  const {color2} = useParams();
+  if(isNaN(input)){
+    return (
+      <h1 style={{background: color1, color: color2}}>The word is: {input}</h1>
+    )
+    console.log(true)
+  }else {
+    return <h1>The number is: { input }</h1>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Switch>
+        <Route  exact path="/Home">
+          <Home />
+        </Route>
+        <Route  exact path="/:input">
+          <Hello />
+        </Route>
+        <Route  exact path="/:input/:color1/:color2">
+          <Hello />
+        </Route>
+        <Route path="/:input">
+          <Hello />
+        </Route>
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
